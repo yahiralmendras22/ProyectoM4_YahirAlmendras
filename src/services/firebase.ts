fb28c5b1fa5fa04583c03d673043f7a723e8db40
yahiralmendras22 <yahiralmendras22@gmail.com>
2026-09-19 13:01:20 -0300

    eliminacion de una linea

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
